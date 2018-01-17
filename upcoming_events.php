0c1599aa0517aad100e8c5f2b6cf4b73b8156c54
Leader, Dylan <dleader@fnb.co.za>
2018-01-17 17:46:09 +0200

    Updated the following: - Updated buttons for upcoming events

--- upcoming_events.php
+++ upcoming_events.php
@@ -37,8 +37,8 @@
             <figure class="mbr-figure container">
                 <div class="w3-center">
                     <div class="w3-section">
-                        <button class="w3-button w3-light-grey" onclick="plusDivs(-1)">❮ Prev</button>
-                        <button class="w3-button w3-light-grey" onclick="plusDivs(1)">Next ❯</button>
+                        <button class="btn btn-form btn-secondary display-7" onclick="plusDivs(-1)">❮ Prev</button>
+                        <button class="btn btn-form btn-secondary display-7" onclick="plusDivs(1)">Next ❯</button>
                     </div>
                 </div>
                 <div class="w3-content w3-display-container w3-section">
