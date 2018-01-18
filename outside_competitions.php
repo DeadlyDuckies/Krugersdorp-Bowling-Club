af01bde32b948ac269a05dad273f081f7c7227cd
Leader, Dylan <dleader@fnb.co.za>
2018-01-18 10:40:14 +0200

    Updated the following: - Added outside competitions

--- outside_competitions.php
+++ outside_competitions.php
@@ -46,6 +46,7 @@
                     <img class="upcomingSlides" src="assets/images/All_Cape_Poster_2018.jpg" style="width:100%">
                     <img class="upcomingSlides" src="assets/images/CBCOB_Shamrock_Challenge_2018.jpg" style="width:100%">
                     <img class="upcomingSlides" src="assets/images/Roosevelt_Park_Easter_Tournament.JPG" style="width:100%">
+                    <img class="upcomingSlides" src="assets/images/Scottburgh_August_Tournament_2018.jpg" style="width:100%">
                 </div>
                 <script>
                     var myIndex = 0;
