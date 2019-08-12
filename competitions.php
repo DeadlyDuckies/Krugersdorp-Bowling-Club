e89b208b31261369261098adefa4ab2b47f84355
Leader, Dylan <dleader@fnb.co.za>
2019-08-12 07:53:21 +0200

    * Updated final club competition results

--- competitions.php
+++ competitions.php
@@ -33,6 +33,91 @@
         </div>
     </section>
 
+    <section class="mbr-section content4 cid-qztSxSN3he" id="content4-20" data-rv-view="130">
+        <div class="container">
+            <div class="media-container-row">
+                <div class="title col-12 col-md-8">
+                    <h3 class="mbr-section-subtitle align-center mbr-light mbr-fonts-style display-5">
+                        Final Results - Competitions for 2018/2019
+                    </h3>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-table cid-qztItQMCpK" id="table1-1t" data-rv-view="84">
+        <div class="container container-table">
+            <div class="table-wrapper">
+                <div class="container scroll">
+                    <table class="table isSearch" cellspacing="0">
+                        <thead>
+                            <tr class="table-heads ">
+                                <th class="head-item mbr-fonts-style display-7">
+                                    COMPETITION
+                                </th>
+                                <th class="head-item mbr-fonts-style display-7">
+                                    WINNER
+                                </th>
+                                <th class="head-item mbr-fonts-style display-7">
+                                    RUNNER UP
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="body-item mbr-fonts-style display-7">Men's Championship Singles</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Jaco van Deventer</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Rodney Gray</td>
+                            </tr>
+                            <tr>
+                                <td class="body-item mbr-fonts-style display-7">Ladies Championship Singles</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Theresa Klein</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Ansie Fourie</td>
+                            </tr>
+                            <tr>
+                                <td class="body-item mbr-fonts-style display-7">Men's Handicap Singles</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Robert Scott</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Kobus Britz</td>
+                            </tr>
+                            <tr>
+                                <td class="body-item mbr-fonts-style display-7">Ladies Handicap Singles</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Kirstin Leader</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Hester Hattingh</td>
+                            </tr>
+                            </tbody>
+                            <tr>
+                                <td class="body-item mbr-fonts-style display-7">Men's Pairs</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Jaco van Deventer & Coenie Pieterse</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Stitch Economakis & Lee Barnes</td>
+                            </tr>
+                            <tr>
+                                <td class="body-item mbr-fonts-style display-7">Ladies Pairs</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Elysses Heine & Belinda Economakis</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Linda Bosch & Hester Hattingh</td>
+                            </tr>
+                            <tr>
+                                <td class="body-item mbr-fonts-style display-7">Mixed Pairs</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Graeme Morton & Bokkie Delport</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Alan Lyall & Eleanor Syme</td>
+                            </tr>
+                            <tr>
+                                <td class="body-item mbr-fonts-style display-7">Men's Novices</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Gavin Heine</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Shane Hallam</td>
+                            </tr>
+                            <tr>
+                                <td class="body-item mbr-fonts-style display-7">Ladies Novices</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Ansie Fourie</td>
+                                <td class="body-item mbr-fonts-style display-7" style="text-align: center">Elysses Heine</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <br>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <div id="competition_data"></div>
 
     <section class="section-table cid-qztItQMCpK" id="table1-1t" data-rv-view="84">
